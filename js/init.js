eaa34b277228e99d9d5c490729628159815f9b1d
Jody Swartz <jodyhswartz@hotmail.com>
2023-05-05 10:08:59 +0200

    Updated Holiday Date

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 // setup timer with set textual date in the future
 const timer1 = new CountdownTimer({
 	selector: "#holidayCountdown",
-	targetDate: new Date("December, 14 2022 00:00:00"),
+	targetDate: new Date("July, 17 2023 00:00:00"),
 });
 
 timer1.startTimer();
