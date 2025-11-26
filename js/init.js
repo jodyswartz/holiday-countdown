2d1948d512e1ccddf54955dd965019c25c6f4386
Jody Swartz <jodyhswartz@gmail.com>
2025-11-26 10:04:13 +0100

    start countdown to xmas holidays 2025

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 // setup timer with set textual date in the future
 const timer1 = new CountdownTimer({
 	selector: "#holidayCountdown",
-	targetDate: new Date("November, 29 2024 17:00:00"),
+	targetDate: new Date("December, 19 2025 17:00:00"),
 });
 
 timer1.startTimer();
